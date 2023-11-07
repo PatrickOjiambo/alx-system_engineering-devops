026d366c663374ac2301cabb6ebe5e914957c5d0
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-07 14:15:59 +0300

    feat:Get subscribers reddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Total number of subscribers"""
+from requests import get
+def number_of_subscribers(subreddit):
+    """Total number of subs
+    Args:
+        subreddit - String
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    result = response.json()
+    try:
+        return result['data']['subscribers']
+
+    except Exception:
+        return 0
+    
+
